1bdb7f89c76c5ea362b00c8f13b7823a725c6062
LaZyFee <rhr277@gmail.com>
2025-08-19 16:44:33 +0600

    models created

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
-
+import { dbConnect } from "@/service/mongo";
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
 
@@ -11,13 +11,14 @@ export const metadata = {
   description: "Explore || Learn || Build || Share",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const conn  = await dbConnect();
   return (
     <html lang="en">
       <body
         className={cn(inter.className, poppins.className)}>
-        {children}
-        <Toaster richColors position="top-center" />
+          {children}
+          <Toaster richColors position="top-center"/>
       </body>
     </html>
   );
